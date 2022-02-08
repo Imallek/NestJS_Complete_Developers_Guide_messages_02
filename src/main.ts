81c65fea2e905a16fd958a6b08158eb48694ebe8
Adeel Shaukat <a.shaukat@ssidecisions.com>
2022-02-08 15:46:23 +0500

    make DTO and apply validation, add prettier and eslint confs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+
+  //We can validate request on even a single request
+  //But we want to validate all the incoming requests so we apply this pipe globally
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
