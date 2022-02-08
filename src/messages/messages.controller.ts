15d882df7d3cf8c98f95e5fcabcfe632049beb0f
Adeel Shaukat <a.shaukat@ssidecisions.com>
2022-02-08 14:33:55 +0500

    accessing Request Data with Decorators

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
@@ -6,8 +6,8 @@ export class MessagesController {
   listMessages() {}
 
   @Post()
-  createMessage() {}
+  createMessage(@Body() body: any) {}
 
-  @Post(':/id')
-  getMessage() {}
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {}
 }
