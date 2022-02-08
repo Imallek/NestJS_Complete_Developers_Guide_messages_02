3d6e3b88e4490568330fe4515714bc53cd45f92d
Adeel Shaukat <a.shaukat@ssidecisions.com>
2022-02-08 16:58:38 +0500

    implement service and repositories for messages

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,16 +1,29 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  // DO NOT DO THIS IN REAL APP, USE DEPENDENCY INJECTION
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    const { content } = body;
+    return this.messagesService.create(content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {}
+  getMessage(@Param('id') id: string) {
+    return this.messagesService.findOne(id);
+  }
 }
